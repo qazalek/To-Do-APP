7581f3fbaddbf15d18aeac995c0aeef6ee95a7d1
qazalek <128769228+qazalek@users.noreply.github.com>
2023-10-09 22:47:58 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,37 +1,39 @@
+import functions
 import PySimpleGUI as Sg
-import functions  # Assuming you have a module named 'functions' with required functions
 
-input_box = Sg.InputText(tooltip="Enter todo", key="todo")
-add_button = Sg.Button("Add")
-list_box = Sg.Listbox(values=functions.get_todos(), key='todos',
-                     enable_events=True, size=[45, 10])
-edit_button = Sg.Button("Edit")
+label=Sg.Text("Type in a to-do")
+input_box=Sg.InputText(tooltip="Enter todo", key="todo")
+add_button= Sg.Button("Add")
+list_box=Sg.Listbox(values=functions.get_todos(), key='todos',
+                    enable_events=True, size=[45, 10])
+edit_button=Sg.Button("Edit")
 
-window = Sg.Window('My To-Do App',
-                  layout=[[input_box, add_button], [list_box, edit_button]],
-                  font=('Helvetica', 20))
+window=Sg.Window('My To-Do App',
+                 layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                 font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
+    event, values=window.read()
+    print(1,event)
+    print(2,values)
+    print(3,values['todos'])
+    match event:
+        case "Add":
+            todos=functions.get_todos()
+            new_todo=values['todo']+ "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case "Edit":
+            todo_to_edit= values['todos'][0]
+            new_todo=values['todo']
 
-    if event == Sg.WIN_CLOSED:
-        break
-
-    if event == "Add":
-        todos = functions.get_todos()
-        new_todo = values['todo'] + "\n"
-        todos.append(new_todo)
-        functions.write_todos(todos)
-
-    if event == "Edit":
-        todo_to_edit = values['todos'][0]  # Get the selected item from the listbox
-        new_todo = values['todo'] + "\n"
-
-        todos = functions.get_todos()
-        index = todos.index(todo_to_edit)
-        todos[index] = new_todo
-        functions.write_todos(todos)
-
-    window['todos'].update(values=functions.get_todos())
-
+            todos=functions.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index]=new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case'todos':
+            window['todo'].update(value=values['todos'][0])
+        case Sg.WIN_CLOSED:
+            break
 window.close()
